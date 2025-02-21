9e1446f4c3294aa9b5a320306bf2fb9afa149884
maloof <maloofbashir70@gmail.com>
2025-02-21 11:02:57 +0530

    adding some more functionality

--- e_mail.py
+++ e_mail.py
@@ -2,7 +2,10 @@ import email
 import imaplib
 from email.header import decode_header
 import os
+import re
+from datetime import datetime
 
+FOLDERS=[]
 SAVE_FOLDER = "attachments"
 if not os.path.exists(SAVE_FOLDER):
     os.makedirs(SAVE_FOLDER)
@@ -21,7 +24,8 @@ email_ids=message[0].split()
 email_ids.reverse()
 
 count=0
-for email_id in email_ids[251:350]:
+print(f"total emails= {len(email_ids)}")
+for email_id in email_ids[:20]:
     if count<50:
         status, msg_data =  mail.fetch(str(email_id, 'utf-8'), "(RFC822)")
         inner_c=0
@@ -30,38 +34,46 @@ for email_id in email_ids[251:350]:
             if isinstance(response_part, tuple):
                 email_body = ""
                 msg = email.message_from_bytes(response_part[1])
+                
+                subject, encoding = decode_header(msg["Subject"])[0]
+                if isinstance(subject, bytes):
+                    subject = subject.decode(encoding if encoding else "utf-8")
 
                 from_email = msg.get("From")  # Sender
+                from_email = re.search(r'<([^>]+)>', from_email).group(1)
                 to_email = msg.get("To")  # Recipient(s)
                 cc_email = msg.get("Cc")  # CC (if available)
                 bcc_email = msg.get("Bcc")  # BCC (if available)
                 date_sent = msg.get("Date")  # Date and time when email was sent
+                mystring=date_sent
+                # date_sent=date_sent.split("(")[0]
+                # date_sent = date_sent.replace(" (UTC)", "")
+                # date_obj = datetime.strptime(date_sent, "%a, %d %b %Y %H:%M:%S %z")
+                # formatted_date = date_obj.strftime("%d-%m-%Y")
+                
                 for part in msg.walk():
                     content_type = part.get_content_type()
                     if content_type == "text/plain":  # Plain text email body
                         email_body += part.get_payload(decode=True).decode("utf-8", errors="ignore")
                 # email_body = msg.get_payload(decode=True).decode("utf-8", errors="ignore")
                     if part.get_content_disposition() == "attachment":
+                        folder_name=from_email
+                        if not os.path.exists(folder_name):
+                            os.makedirs(folder_name)
                         filename = part.get_filename()
                         if filename:
                             filename, encoding = decode_header(filename)[0]
                             if isinstance(filename, bytes):
                                 filename = filename.decode(encoding if encoding else "utf-8")
-
-                            filepath = os.path.join(SAVE_FOLDER, filename)
+                            filepath = os.path.join(folder_name, subject)
                             with open(filepath, "wb") as f:
                                 f.write(part.get_payload(decode=True))
                             print(f"Saved attachment: {filename}")
 
-                subject, encoding = decode_header(msg["Subject"])[0]
-                if isinstance(subject, bytes):
-                    subject = subject.decode(encoding if encoding else "utf-8")
 
-                # print(f"From: {from_email}")
-                # print(f"date_sent: {date_sent}")
                
 
-                print(f"Processing email..... {subject}")
+                print(f"Processing email..... {subject} from email dated {mystring}")
                 count=count+1
     else:
         break
